fcfba0191a6d67386c08b3ad829666ac9a6af848
Lucas Turci <lucas.turci@gmail.com>
2018-07-04 23:20:58 -0300

    Adicionei comentarios ao a.cpp

--- dia1/a.cpp
+++ dia1/a.cpp
@@ -1,39 +1,38 @@
 #include <bits/stdc++.h>
 
-#define pb push_back
-#define fi first
-#define se second
-#define mp make_pair
-
 using namespace std;
 const int N = 1000007, inf = 0x3f3f3f3f;
 
-typedef pair<int, int> ii;
-typedef long long ll;
+int v[N], f[N][2], n;
 
+/*
+	f[p][odd] = soma da subsequencia que da a maior soma impar (se odd = 1) ou par (se odd = 0) para o sufixo p...n-1
 
-int v[N], memo[N][2];
-int n;
+	f[p][odd] = max(f[p+1][x] + v[p], f[p+1][odd]), em que x = !odd se v[p] for impar e odd se v[p] for par
+	f[n][odd] = -inf, se odd = 1 ou 0, se odd = 0
 
+	Solucao melhor:
+	Pegue todos os elementos positivos, e se a soma for par, a melhor subsequencia eh a gerada 
+	tirando-se um elemento impar positivo do conjunto (o menor deles) ou adicionando um elemento 
+	impar negativo ao conjunto (o menor em valor absoluto)
+*/
 int solve(int p, int odd) {
-	if(p == n) {
-		if(odd == 0) return -inf;
-		else return 0;
-	}
+	if(p == n)
+		return odd? 0 : -inf;
 
-	int & st = memo[p][odd];
+	int & st = f[p][odd];
 
 	if(st == inf) {
-		st = max(v[p] + solve(p+1, (odd + (v[p]%2+2)%2)%2 ), solve(p+1, odd));
+		st = max(v[p] + solve(p+1, v[p]%2? !odd : odd), solve(p+1, odd));
 	}	
 
 	return st;
 }
 
 int main() {
-	memset(memo, inf, sizeof memo);
 	ios::sync_with_stdio(false);
 	
+	memset(f, inf, sizeof f);
 	cin >> n;
 
 	for(int i = 0; i < n; ++i) {
